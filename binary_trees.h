97f150845c96fe9f2581d6151b702dc9754ef051
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-03-01 22:29:57 +0100

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -36,6 +36,7 @@ typedef struct binary_tree_s avl_t;
 void binary_tree_print(const binary_tree_t *);
 
 /* Prototypes */
+ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
